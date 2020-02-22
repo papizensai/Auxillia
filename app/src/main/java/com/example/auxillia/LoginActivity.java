32b341efbb96252f50f219adbc4fce619d6cb1bb
github <jhumphrey33@yahoo.com>
2020-02-22 17:45:34 -0600

    login & reg

--- app/src/main/java/com/example/auxillia/LoginActivity.java
+++ app/src/main/java/com/example/auxillia/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.auxillia;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,6 +22,26 @@ public class LoginActivity extends AppCompatActivity
         setContentView(R.layout.activity_login);
 
         //linking variables to buttons & text
-        loginEmail
+        loginEmailText = findViewById(R.id.loginEmail);
+        loginBttn = findViewById(R.id.loginBttn);
+        loginRegBttn = findViewById(R.id.loginRegBttn);
+
+        // on click triggers register method to send user to register page
+        loginRegBttn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
+
+    }
+
+    //sends user to register page
+    private void Register()
+    {
+        Intent registerIntent = new Intent (LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
+        finish();
     }
 }
--- app/src/main/java/com/example/auxillia/RegisterActivity.java
+++ app/src/main/java/com/example/auxillia/RegisterActivity.java
@@ -3,11 +3,18 @@ package com.example.auxillia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    //declaring fields and buttons
+    private EditText loginEmailText;
+    private Button loginBttn, loginRegBttn;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
