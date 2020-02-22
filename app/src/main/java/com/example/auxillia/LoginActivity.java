918cc2bfae71520253004b835d73e8e7d13dd68d
github <jhumphrey33@yahoo.com>
2020-02-22 17:24:48 -0600

    entry level

--- app/src/main/java/com/example/auxillia/LoginActivity.java
+++ app/src/main/java/com/example/auxillia/LoginActivity.java
@@ -1,11 +1,25 @@
 package com.example.auxillia;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity
 {
+    //declaring fields and buttons
+    private EditText loginEmailText;
+    private Button loginBttn, loginRegBttn;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
+        //linking variables to buttons & text
+        loginEmail
+    }
 }
--- app/src/main/java/com/example/auxillia/MainActivity.java
+++ app/src/main/java/com/example/auxillia/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.auxillia;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //declaring fields and buttons
+    private Button logout;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //linking variables to buttons & text
+        logout = findViewById(R.id.logoutBttn);
+    }
+
+   @Override
+   protected void onStart()
+   {
+       super.onStart();
+
+       logout.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               logOut();
+           }
+       });
+   }
+
+    // will sign user out and send to login
+    // to login currently
+    private void logOut()
+    {
+        Intent loginIntent = new Intent (MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
     }
 }
